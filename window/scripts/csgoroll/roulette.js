17fb6183773a8bc056c7063922bb89695eb63d03
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2017-06-26 15:11:10 -0300

    Refactor CSGORoll script

--- window/scripts/csgoroll/roulette.js
+++ window/scripts/csgoroll/roulette.js
@@ -11,49 +11,63 @@ chrome.storage.local.get(function(data) {
   }
 });
 
+function preventLogout() {
+  window.location.reload();
+}
+
+function tryAgainLater() {
+  setTimeout(loop, 2000);
+}
+
+function calculateBet() {
+  if ($('csgr-round-list .round.green').length > 0) {
+    shouldTrack = true;
+
+    if (timesWithNo0 !== 0) {
+      if (shouldTrack) {
+        analytics[timesWithNo0] = (analytics[timesWithNo0] || 0) + 1;
+        chrome.storage.local.set({analytics: analytics});
+      }
+
+      if (bet !== 0) {
+        chrome.runtime.sendMessage({
+          title: 'Congratulations!',
+          message: 'You just won $' + (14 * bet) + ' and the current balance is $' + balance + ' on CSGORoll'
+        });
+        bet = 0;
+      }
+    }
+
+    timesWithNo0 = 0;
+  } else {
+    timesWithNo0 += 1;
+  }
+
+  if (timesWithNo0 >= 10 && timesWithNo0 < 35) {
+    bet = greed * Math.floor(timesWithNo0 / 10) / 100;
+  } else {
+    bet = 0;
+  }
+}
+
+function doBet() {
+  $('[formcontrolname="bet"]').val(bet)[0].dispatchEvent(new Event('change'));
+  $('.btn-3d-success').click();
+}
+
 function loop() {
   var balance = parseFloat($('.btn-balance').text().trim());
 
   if (Date.now() - openAt > 3600000 && timesWithNo0 === 0) {
-    /* prevent logout */
-    window.location.reload();
+    preventLogout();
   } else if ($('csgr-countdown .progress').width() < 1) {
-    /* is spinning */
-    setTimeout(loop, 2000);
+    tryAgainLater();
   } else {
-    /* check if should bet */
-    if ($('csgr-round-list .round.green').length > 0) {
-      shouldTrack = true;
+    calculateBet();
 
-      if (timesWithNo0 !== 0) {
-        if (shouldTrack) {
-          analytics[timesWithNo0] = (analytics[timesWithNo0] || 0) + 1;
-          chrome.storage.local.set({analytics: analytics});
-        }
-
-        if (bet !== 0) {
-          chrome.runtime.sendMessage({
-            title: 'Congratulations!',
-            message: 'You just won $' + (14 * bet) + ' and the current balance is $' + balance + ' on CSGORoll'
-          });
-          bet = 0;
-        }
-      }
-
-      timesWithNo0 = 0;
-    } else {
-      timesWithNo0 += 1;
-    }
-
-    if (timesWithNo0 >= 10 && timesWithNo0 < 35) {
-      bet = Math.min(greed * Math.floor(timesWithNo0 / 10) / 100, balance);
-      if (bet > 0) {
-        balance -= bet;
-        $('[formcontrolname="bet"]').val(bet)[0].dispatchEvent(new Event('change'));
-        $('.btn-3d-success').click();
-      }
-    } else {
-      bet = 0;
+    if (bet > 0) {
+      balance -= bet;
+      doBet();
     }
 
     console.log('times: ' + timesWithNo0 + ', balance: ' + balance + ', bet: ' + bet);
