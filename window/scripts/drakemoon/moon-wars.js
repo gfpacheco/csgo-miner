aa4b4358c430aaa108c3f1e5306c63be655cdc5d
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2017-06-26 14:58:49 -0300

    Refactor Drakemoon script

--- window/scripts/drakemoon/moon-wars.js
+++ window/scripts/drakemoon/moon-wars.js
@@ -1,40 +1,67 @@
 var openAt = Date.now();
 
+function randomSeconds(from, to) {
+  return (from + (Math.random() * (to - from))) * 1000;
+}
+
+function preventLogout() {
+  window.location.reload();
+}
+
+function claimDiamonds(claimButton) {
+  claimButton.click();
+  setTimeout(loop, randomSeconds(1, 2));
+}
+
+function tryAgainLater() {
+  setTimeout(loop, randomSeconds(60, 120));
+}
+
+function login(loginButton) {
+  loginButton[0].click();
+}
+
+function openChest(numDiamonds) {
+  $('.chest').eq(getChestIndex(numDiamonds)).click();
+  setTimeout(sellPrize, randomSeconds(9, 10));
+}
+
+function getChestIndex(numDiamonds) {
+  if (numDiamonds > 5000) return 3;
+  if (numDiamonds > 3000) return 2;
+  if (numDiamonds > 1000) return 1;
+  return 0;
+}
+
+function sellPrize() {
+  var result = $('#chest-open-result .name-wrapper span').hide().show(0).eq(0).text();
+
+  if (result.indexOf('Not your day') === -1 && result.indexOf('Chest') === -1) {
+    chrome.runtime.sendMessage({
+      title: 'Congratulations!',
+      message: 'You just got a ' + result + ' on Drakemoon'
+    });
+  }
+
+  $('.icon-arrow-circle-o-up').click();
+  setTimeout(loop, randomSeconds(1, 2));
+}
+
 function loop() {
   var claimButton = $('.free-bar .icon-free_coins');
   var numDiamonds = parseInt($('.user-stats .diamonds span').text(), 10);
   var loginButton = $('.login-button');
 
   if (Date.now() - openAt > 3600000) {
-    /* prevent logout */
-    window.location.reload();
+    preventLogout();
   } else if (claimButton.length > 0) {
-    /* claim diamonds */
-    claimButton.click();
-    setTimeout(loop, (1 + Math.random()) * 1000);
+    claimDiamonds(claimButton);
   } else if (numDiamonds < 100) {
-    /* not enough diamonds */
-    setTimeout(loop, (1 + Math.random()) * 60000);
+    tryAgainLater();
   } else if (loginButton.length > 0) {
-    /* login */
-    loginButton[0].click();
+    login(loginButton);
   } else {
-    /* open chest */
-    $('.chest').eq(0).click();
-    setTimeout(function() {
-      /* sell prize */
-      var result = $('#chest-open-result .name-wrapper span').hide().show(0).eq(0).text();
-
-      if (result.indexOf('Not your day') === -1 && result.indexOf('Chest') === -1) {
-        chrome.runtime.sendMessage({
-          title: 'Congratulations!',
-          message: 'You just got a ' + result + ' on Drakemoon'
-        });
-      }
-
-      $('.icon-arrow-circle-o-up').click();
-      setTimeout(loop, (1 + Math.random()) * 1000);
-    }, (9 + Math.random()) * 1000);
+    openChest(numDiamonds);
   }
 }
 
